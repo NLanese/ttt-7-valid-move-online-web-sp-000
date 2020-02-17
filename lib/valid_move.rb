ea55ea647f17ed6597fc5589d7fb68dca922eb64
Nick Lanese <nlanese21@gmail.com>
2020-02-17 17:22:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 def valid_move?(board, input)
   if (input > 9 || input < 1)
     return false
-  elsif !(board[input] == " " && board[input] == "" && board[input] == nil) 
+  elsif !(board[input] == " " && board[input] == "" && board[input] == nil)
     return false
-  else 
+  else
     return true
   end
 end
